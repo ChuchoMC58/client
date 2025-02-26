a1c4d05246320996eb3edede702be2148f9a7257
Santos de Jesús Mateo Camacho <smateo@epica.solutions>
2025-02-25 20:34:49 -0700

    End of section10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,25 +4,15 @@ import { HeaderComponent } from "./layout/header/header.component";
 import { HttpClient } from '@angular/common/http';
 import { Product } from './shared/models/product';
 import { Pagination } from './shared/models/pagination';
+import { ShopService } from './core/services/shop.service';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  baseUrl = 'https://localhost:5001/api/';
-  private http = inject(HttpClient);
-  products: Product[] = [];
-
+export class AppComponent {
   title = 'client';
-
-  ngOnInit(): void {
-    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-      next: (res) => this.products = res.data,
-      error: (error) => console.error(error),
-      complete: () => console.log('complete')
-    })
-  }
 }
