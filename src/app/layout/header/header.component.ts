49c3136eeb2875977928bb39357e1954a72a33bb
Santos de Jesús Mateo Camacho <smateo@epica.solutions>
2025-03-31 21:32:49 -0600

    End of section 13

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatButton } from '@angular/material/button';
 import { MatBadge } from '@angular/material/badge';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatProgressBar } from '@angular/material/progress-bar';
+import { BusyService } from '../../core/services/busy.service';
+import { CartService } from '../../core/services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -11,11 +14,13 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
     MatButton,
     MatBadge,
     RouterLink, 
-    RouterLinkActive
+    RouterLinkActive,
+    MatProgressBar
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  busyService = inject(BusyService);
+  cartService = inject(CartService);
 }
